a2f02fb4ef809600d39ee951a52a5355e73ac7d7
Meu Nome <danielavieira6689@gmail.com>
2019-03-20 20:16:38 -0300

    Primeiro oi

---
+++ oi.py
@@ -0,0 +1,31 @@
+print ("Parabens pra mim")
+print ('''
+To the dance floor
+        o/
+       /|
+        |
+       / \
+ ''')
+print ("hue"*3)
+print ("Eu" + "sou"+ "br" + "!!" *5)
+
+print ("Ei parabens")
+
+print ("Aqui vai um dinossauro")
+
+
+print('''
+                         __
+                        / _)
+                 .-^^^-/ /
+              __/       /
+             <__.|_|"|_|
+''')
+print (" ." *10)
+print (" i" *10)
+print ("#" *21)
+print ("=" *21)
+print ("#" *21)
+print ("=" *21)
+print ("#" *21)
+                       
